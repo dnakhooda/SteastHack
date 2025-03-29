d6fc5b03f4d70fe0782230b7e72dfac49cbb7589
Giovanni <limena.g@northeastern.edu>
2025-03-29 15:26:07 -0400

    firebase data structure implementation

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -13,9 +13,16 @@ const firebaseConfig = {
   storageBucket: "steast-hackathon.firebasestorage.app",
   messagingSenderId: "926165094246",
   appId: "1:926165094246:web:98b4477669b8ab841bc2a7",
-  measurementId: "G-XTQ2Q6BSW6"
+  measurementId: "G-XTQ2Q6BSW6",
+  databaseURL: "https://steast-hackathon-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
